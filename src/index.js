508e45d54c90942bbc6be5438ffb931d1260e613
pnia0 <tryo2357@gmail.com>
2025-05-08 22:42:32 +0900

    modify appearance

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const test_text = '# fandation of <br>podman\nリポジトリからコンテナをプルし、実行。\nリポジトリやsystemdからコンテナの自動起動と管理を行える。\n<img width="300px" src="./materials/podman-pod-image.png"><br>\n\n## related software\n- Buildah: ゼロから、あるいはイメージからコンテナを構築\n- Skepeo: 様々な環境間でのコンテナイメージのコピー\n- runC: OCIランタイム。イメージのビルドと実行、Docker形式のイメージの実行\n- CRI-O: Kubernetesとの連携\n\n## podman with cubernetes\npodmanではkubernetesYAMLの読み込みや、実行中のpodからの書き出しに対応している。\n読み込み: `podman kube play "pod.yaml"`\n書き出し: `podman generate kube -f path target_pod`\n\n# installation\n[about installation](https://github.com/containers/podman/blob/main/docs/tutorials/podman-for-windows.md)\n`podman machine init`\n\n# operate podman\nstart podman: `podman machine start`\nstop podman: `podman machine stop`\nchange to rootful: `podman machine set --rootful`\nchange to rootless: `podman machine set --rootless`(たぶん)\nlist podman machine(s): `podman machine ls`\n`podman system connection list`\nremove a machine: `podman machine rm "Option<machine_name>"`\n\n## container\nstart container: `podman run --name "container_name" "image_name"`\nstop container: `podman stop "container_name"`\nremove container: `podman rm "container_name"`\nlist running container: `podman ps`\nlist any containers: `podman ps -a`\n## image\npull image: `podman pull "image_name"`\nremove image: `podman rmi "image_id"`\nlist images: `podman images`\n## pod\nmake new pod: `podman pod create -p 8080:80 --name "pod_name"`\n### start pod:\n`podman run "pod_name"`\nport_forwarding: `podman run --rm -d -p 8080:80 "pod_name"`\nvolume_mounting: `podman run --rm -v PATH_on_windows:targetPATH_on_pod`\nstop pod: `podman pod stop "pod_name"`\nremove pod: `podman pod rm "pod_name"`\npod一覧表示: `podman pod ls`\n`podman pod ps`\nshow pod state: `podman pod top "pod_name"`\nshow pod state in detail: `podman pod inspect "pod_name"`\n## manage container in pod\nstart container: `podman run -dt --pod "pod_name" --name "container_name" "image_name"`\n# macvlan: podman netwark\n';
+const test_text = '\n# fandation of podman\nリポジトリからコンテナをプルし、実行。\nリポジトリやsystemdからコンテナの自動起動と管理を行える。\n<img width="300px" src="./materials/podman-pod-image.png"><br>\n\n## related software\n- Buildah: ゼロから、あるいはイメージからコンテナを構築\n- Skepeo: 様々な環境間でのコンテナイメージのコピー\n- runC: OCIランタイム。イメージのビルドと実行、Docker形式のイメージの実行\n- CRI-O: Kubernetesとの連携\n\n## podman with cubernetes\npodmanではkubernetesYAMLの読み込みや、実行中のpodからの書き出しに対応している。\n読み込み: `podman kube play "pod.yaml"`\n書き出し: `podman generate kube -f path target_pod`\n\n# installation\n[about installation](https://github.com/containers/podman/blob/main/docs/tutorials/podman-for-windows.md)\n`podman machine init`\n\n# operate podman\nstart podman: `podman machine start`\nstop podman: `podman machine stop`\nchange to rootful: `podman machine set --rootful`\nchange to rootless: `podman machine set --rootless`(たぶん)\nlist podman machine(s): `podman machine ls`\n`podman system connection list`\nremove a machine: `podman machine rm "Option<machine_name>"`\n\n## container\nstart container: `podman run --name "container_name" "image_name"`\nstop container: `podman stop "container_name"`\nremove container: `podman rm "container_name"`\nlist running container: `podman ps`\nlist any containers: `podman ps -a`\n## image\npull image: `podman pull "image_name"`\nremove image: `podman rmi "image_id"`\nlist images: `podman images`\n## pod\nmake new pod: `podman pod create -p 8080:80 --name "pod_name"`\n### start pod:\n`podman run "pod_name"`\nport_forwarding: `podman run --rm -d -p 8080:80 "pod_name"`\nvolume_mounting: `podman run --rm -v PATH_on_windows:targetPATH_on_pod`\nstop pod: `podman pod stop "pod_name"`\nremove pod: `podman pod rm "pod_name"`\npod一覧表示: `podman pod ls`\n`podman pod ps`\nshow pod state: `podman pod top "pod_name"`\nshow pod state in detail: `podman pod inspect "pod_name"`\n## manage container in pod\nstart container: `podman run -dt --pod "pod_name" --name "container_name" "image_name"`\n# macvlan: podman netwark\n';
+const LINE_NUMBER_OFFSET = "60px";
+const TEXT_AREA_OFFSET = "0.5em";
 
 class Editor {
     #buffer
@@ -93,13 +95,14 @@ class Buffer {
 }
 
 class View {
-    #headView;
-    #cursorLineViewArea;
-    #cursorLineNumberView;
-    #cursorLineNumber;
-    #cursorLineView;
-    #bottomView;
+    #headView = "";
+    #cursorLineViewArea = null;
+    #cursorLineNumberView = "1";
+    #cursorLineNumber = 1;
+    #cursorLineView = "";
+    #bottomView = "";
     constructor(editor){
+        editor.style.width = "100%";
         this.#headView = document.createElement("div");
         this.#cursorLineViewArea = document.createElement("div");
         this.#cursorLineView = document.createElement("div");
@@ -112,11 +115,18 @@ class View {
         this.#cursorLineViewArea.appendChild(this.#cursorLineView);
         this.#cursorLineViewArea.style.display = "flex";
         this.#cursorLineViewArea.style.flexDirection = "row";
-        this.#cursorLineNumberView.style.width = "30px";
+        this.#cursorLineViewArea.style.width = "100%";
+        this.#cursorLineNumberView.style.width = LINE_NUMBER_OFFSET;
+        this.#cursorLineNumberView.style.textAlign = "right";
         this.#headView.style.whiteSpace = "pre-line";
         this.#bottomView.style.whiteSpace = "pre-line";
+        this.#headView.style.marginLeft = LINE_NUMBER_OFFSET;
+        this.#bottomView.style.marginLeft = LINE_NUMBER_OFFSET;
+        this.#headView.style.paddingLeft = TEXT_AREA_OFFSET;
+        this.#cursorLineView.style.paddingLeft = TEXT_AREA_OFFSET;
+        this.#bottomView.style.paddingLeft = TEXT_AREA_OFFSET;
         this.#headView.style.width = "100%";
-        this.#cursorLineView.style.width = "100%";
+        this.#cursorLineView.style.flexGrow = "1";
         this.#bottomView.style.width = "100%";
         this.#headView.style.border= "2px solid #ff0000";
         this.#cursorLineView.style.border= "2px solid #ffff00";
